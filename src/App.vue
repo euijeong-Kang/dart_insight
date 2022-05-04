7570d4ef4d962cd3e32ce62018e459db6fac95c9
euijeong-Kang <dmlwjd558@gmail.com>
2022-05-04 16:32:21 +0900

    화면 수정

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 #app {
-  width: 450px;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
